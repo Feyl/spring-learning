aa2f3dc3be14bb7500ddeadb13debbc020f99eee
Feyl <490117522@qq.com>
2022-08-05 11:18:38 +0800

    Design and implement a resource loader that parses and registers Bean objects from spring.xml

--- step-05/src/main/java/com/feyl/spring/beans/factory/support/AbstractBeanFactory.java
+++ step-05/src/main/java/com/feyl/spring/beans/factory/support/AbstractBeanFactory.java
@@ -14,17 +14,17 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
 
     @Override
     public Object getBean(String name) throws BeansException {
-        return null;
+        return doGetBean(name, null);
     }
 
     @Override
     public Object getBean(String name, Object... args) throws BeansException {
-        return null;
+        return doGetBean(name, args);
     }
 
     @Override
     public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
-        return null;
+        return (T) getBean(name);
     }
 
     protected <T> T doGetBean(final String name, final Object[] args) {
